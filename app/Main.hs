9862a6a64d65dd2c6f45dc09414043fa96cbc3dc
Roman Treutlein <roman@trueagi.io>
2025-11-04 14:19:29 +0100

    merge executables

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,33 @@ import qualified Control.Monad.State as M
 import Iso
 import OpenCog.MeTTa.Parser
 import System.Environment
+import System.Exit (exitFailure)
+import System.IO (hPutStrLn, stderr)
+
+data Mode = ToMeTTa | ToHSM
 
 main :: IO ()
 main = do
-    [path] <- getArgs
+    args <- getArgs
+    case args of
+        ["to-metta", path] -> runConversion ToMeTTa path
+        ["to-hsm", path] -> runConversion ToHSM path
+        _ -> usage
+
+runConversion :: Mode -> FilePath -> IO ()
+runConversion mode path = do
     file <- readFile path
-    case M.evalStateT (apply parseFile ()) (State {sText = file}) of
-        Right as -> case M.execStateT (unapply parseMeTTa as) (State {sText = ""}) of
-                        Right s -> putStrLn $ sText s
-                        Left e -> print e
+    let (parser, unparser) = case mode of
+            ToMeTTa -> (parseFile, parseMeTTa)
+            ToHSM -> (parseMeTTa, parseFile)
+    case M.evalStateT (apply parser ()) (State {sText = file}) of
+        Right value ->
+            case M.execStateT (unapply unparser value) (State {sText = ""}) of
+                Right s -> putStrLn $ sText s
+                Left e -> print e
         Left e -> print e
+
+usage :: IO ()
+usage = do
+    hPutStrLn stderr "Usage: hsmetta (to-metta|to-hsm) <path>"
+    exitFailure
